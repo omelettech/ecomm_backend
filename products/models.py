1dd0a04b2f8114e5e7825a209f2a6244ad2e2f10
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-13 11:46:43 -0500

    added products model

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Product(models.Model):
+    # id=auto_gen
+    name = models.CharField(max_length=255, null=False, blank=False)  # blank is for forms, null is for database level
+    description = models.TextField(blank=True)  # '' blank string for empty form submission
+    summary = models.CharField(max_length=255, blank=True)
+    # cover
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+
+# Different attributes
+
+class ProductAttribute(models.Model):
+    # id
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    # override this value in the child classes
+    value = models.CharField(blank=False)
+
+    class Meta:
+        abstract = True
+
+
+class SizeAttribute(ProductAttribute):
+    value = models.CharField(blank=False, max_length=256)
+
+
+class ColorAttribute(ProductAttribute):
+    value = models.CharField(blank=False, max_length=256)
+
+
+# for every new attribute table add an attr id to ProductSkus table
+class ProductSkus(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=55, blank=False, null=False)
+    price = models.FloatField(default=0, blank=False)
+    quantity = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+# Create your models here.
