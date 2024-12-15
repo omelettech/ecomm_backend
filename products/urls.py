dde011b8145758d2d0684ce14547abdc155ab001
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-15 02:12:49 -0500

    Added endpoints for ProductSKU

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from products.views import ProductView
+from products.views import *
 
 # HOST:PORT/products/v1/
 urlpatterns = [
-    path('v1/', ProductView.as_view()),
-    path('v1/<int:product_id>', ProductView.as_view())
+    path('v1/products', ProductView.as_view()),
+    path('v1/products/<int:product_id>', ProductView.as_view()),
+
+    path('v1/product_skus/',ProductSkuListCreateAPIView.as_view()),
+    path("v1/product_skus/<int:id>",ProductSkuUpdateDestroyAPIView.as_view())
+
 
 ]
