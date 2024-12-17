6c3a334ccc608b0ff8bfbbd0b21aa43c5b53c9ce
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-17 17:34:57 -0500

    added orders app

--- ecomm_backend/settings.py
+++ ecomm_backend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #my added apps
     'rest_framework',
-    "products"
+    "products",
+    "orders"
+
 ]
 
 MIDDLEWARE = [
