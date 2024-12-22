7d817a04eca2f251b5ec4f5f7e9ca529f37e320b
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-22 15:17:00 -0500

    Added admin support for all apps

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import *
+
+admin.site.register(Product)
+admin.site.register(SizeAttribute)
+admin.site.register(ColorAttribute)
+admin.site.register(ProductSku)
