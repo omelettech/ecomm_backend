dbbeaf092fe236f7e4aec3c3864f6704d5220587
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-22 15:15:40 -0500

    Added orderitem url

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from orders.views import *
 
 urlpatterns = [
-    path('v1/orders', OrderItemListCreateApiView.as_view(), name="order_view"),
-    path('v1/orders/<int:order_id>', OrderRetrieveUpdateDeleteApiView.as_view())
+    path('v1/order', OrderListCreateApiView.as_view(), name="order_view"),
+    path('v1/order/<int:order_id>', OrderRetrieveUpdateDeleteApiView.as_view()),
+
+    path('v1/order_item', OrderItemListCreateApiView.as_view(), name="order_view"),
+    path('v1/order_item/<int:order_item_id>', OrderItemRetrieveUpdateDelete.as_view())
 ]
