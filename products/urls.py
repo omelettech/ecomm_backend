f17ad0f8303c64856563f83036530b2d43338a21
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-23 23:14:13 -0500

    blank

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,8 @@ from products.views import *
 # HOST:PORT/products/v1/
 urlpatterns = [
     path('v1/products', ProductView.as_view(),name="product_view"),
-    path('v1/products/<int:product_id>', ProductView.as_view()),
-    path('v1/products/with_default',get_products_with_default_variations),
+    path('v1/products/<int:product_id>', ProductView.as_view(),name="product_view"),
+    path('v1/products/with_default',get_products_with_default_variations,name="product_default_function"),
 
     path('v1/product_skus/',ProductSkuListCreateAPIView.as_view()),
     path("v1/product_skus/<int:id>",ProductSkuUpdateDestroyAPIView.as_view()),
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
-
+class Customer(models.Model):
+    pass
 
 # Create your models here.
