5ce11d82d7d5fd96eb353edfd546e1d791bd512d
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-25 12:21:14 -0500

    Changed ForeignKey to OneToOne field

--- payments/models.py
+++ payments/models.py
@@ -5,25 +5,27 @@ from orders.models import Order
 
 
 class Payment(models.Model):
-    #id
-    order=models.ForeignKey(Order,on_delete=models.CASCADE,unique=True)
-    total_amount=models.IntegerField(null=False,blank=False,validators=[MinValueValidator(0)])
-    status=models.CharField(max_length=50,choices=
-    [("Pending","Pending"),
-     ("Complete","Complete")],default="Pending")
-    created_at=models.DateTimeField(auto_now_add=True)
-    edited_at=models.DateTimeField(auto_now=True)
-    deleted_at=models.DateTimeField(blank=True,null=True,default=None)
-    currency=models.CharField(max_length=4,default="CAD")
+    # id
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, unique=True)
+    total_amount = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
+    status = models.CharField(max_length=50, choices=
+    [("Pending", "Pending"),
+     ("Complete", "Complete")], default="Pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True, default=None)
+    currency = models.CharField(max_length=4, default="CAD")
+
 
 class Transaction(models.Model):
-    #id
-    payment=models.ForeignKey(Payment,on_delete=models.CASCADE)
+    # id
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
     # customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    payment_method=models.CharField(max_length=50,choices=
-    [("Credit Card","Credit Card"),
-     ("Debit Card","Debit Card"),
-     ("Paypal","Paypal")])
+    payment_method = models.CharField(max_length=50, choices=
+    [("Credit Card", "Credit Card"),
+     ("Debit Card", "Debit Card"),
+     ("Paypal", "Paypal"),
+     ("Stripe", "Stripe")])
+    transaction_id = models.CharField(max_length=50)
 
 # Create your models here.
-
