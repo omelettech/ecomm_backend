78888f2419e700895ee5544cc93dd367c0a942e6
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-27 14:51:57 -0500

    Added wishlist and wishlist item endpoints

--- users/views.py
+++ users/views.py
@@ -6,8 +6,8 @@ from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from users.models import Customer, Wishlist
-from users.serializers import CustomerSerializer, WishlistSerializer
+from users.models import Customer, Wishlist, WishlistItem
+from users.serializers import CustomerSerializer, WishlistSerializer, WishlistItemSerializer
 
 
 class GoogleLogin(SocialLoginView):  # if you want to use Authorization Code Grant, use this
@@ -40,19 +40,49 @@ class CustomerView(generics.RetrieveUpdateDestroyAPIView):
         return JsonResponse(status=200, data={'message': 'Customer deleted successfully.'})
 
 
-class WishlistListCreateView(generics.ListCreateAPIView):
-    queryset = Wishlist.objects.all()
+class WishlistView(generics.RetrieveAPIView):
     serializer_class = WishlistSerializer
+
+    def get_object(self):
+        return Wishlist.objects.get(customer=self.request.user.customer)
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(customer=self.request.user.customer)
+
+
+class WishlistItemView(generics.ListCreateAPIView, generics.RetrieveDestroyAPIView):
+    queryset = WishlistItem.objects.all()
+    serializer_class = WishlistItemSerializer
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access
+
+    def get_queryset(self):
+        return WishlistItem.objects.filter(wishlist=self.request.user.customer.wishlist)
+
+    def get_object(self):
+        return WishlistItem.objects.get(pk=self.kwargs['pk'])
 
     def perform_create(self, serializer):
-        serializer.save(customer=self.request.user.customer)
-
-
-class WishlistRUDView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Wishlist.objects.all()
-    serializer_class = WishlistSerializer
+        """
+        This method overrides the default `perform_create` to save the user-specific wishlist item data.
+        """
+        serializer.save(wishlist=self.request.user.customer.wishlist)
 
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.soft_delete()
-        return JsonResponse(status=200, data={'message': 'Wishlist deleted successfully.'})
+        return JsonResponse(status=200, data={'message': 'Wishlist Item deleted successfully.'})
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if pk:
+            # get a specific wishlist item
+            if WishlistItem.objects.get(pk=pk).wishlist.customer.user == request.user:
+                return self.retrieve(request, *args, **kwargs)
+            return JsonResponse(status=403, data={'message': 'You are not authorized to view this wishlist item.'})
+        else:
+            # get all wishlist items for the user
+            return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        request.data['wishlist'] = request.user.customer.wishlist.id
+        return self.create(request, *args, **kwargs)
+
