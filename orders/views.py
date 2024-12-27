96f5969faeac7f4422ace0b29cfd72e9be28b198
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-12-27 18:28:20 -0500

    changed order logic

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
+import random
+import string
+
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from orders.models import Order, OrderItem
 from orders.serializers import OrderSerializer, OrderItemSerializer
@@ -9,6 +13,12 @@ class OrderListCreateApiView(generics.ListCreateAPIView):
     # CREATE AND RETRIEVE
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    def perform_create(self, serializer):
+        unique_suffix = ''.join(random.choices(string.digits, k=8))  # Random 8-digit suffix
+        order_id = f"ORD#{unique_suffix}"
+        serializer.save(user=self.request.user,
+                        order_number=order_id)
 
 class OrderRetrieveUpdateDeleteApiView(generics.RetrieveUpdateDestroyAPIView):
     # GET PUT DELETE
