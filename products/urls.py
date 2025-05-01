79a872011c2458eb8d0948a2a8eecd5ffa566253
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-05-01 12:42:26 -0400

    Refactored and added trailing slashes

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,10 @@ from products.views import *
 
 # HOST:PORT/products/v1/
 urlpatterns = [
-    path('v1/products', ProductView.as_view(),name="product_view"),
+    path('v1/products/', ProductView.as_view(),name="product_view"),
     path('v1/products/<int:product_id>', ProductView.as_view(),name="product_view"),
-    path('v1/products/with_default',get_products_with_default_variations,name="product_default_function"),
-    path('v1/products/featured',get_featured_products,name="product_featured_function"),
+    path('v1/products/with_default/',get_products_with_default_variations,name="product_default_function"),
+    path('v1/products/featured/',get_featured_products,name="product_featured_function"),
 
     path('v1/product_skus/',ProductSkuListCreateAPIView.as_view()),
     path("v1/product_skus/<int:id>",ProductSkuUpdateDestroyAPIView.as_view()),
