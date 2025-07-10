bd966c184080ad5a8de47d766a9a3bcf9fcd25c9
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-07-09 23:59:41 -0400

    Added url

--- pictures/urls.py
+++ pictures/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from pictures.views import GalleryItemViewSet
+from pictures.views import GalleryItemViewSet, ImageViewSet
 
 router = DefaultRouter()
-router.register(r'GalleryItem',GalleryItemViewSet)  # This will generate the CRUD endpoints
+router.register(r'galleryItem',GalleryItemViewSet)  # This will generate the CRUD endpoints
+router.register(r"images",ImageViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),  # Include the router-generated URLs
